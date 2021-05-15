35c91e36df5c85926b86c893d59c5c9bb3275bb7
Nuri B <g041703@uni-siegen.de>
2021-05-15 23:28:45 +0200

    Projektdateien hinzufügen.

---
+++ Athenathon_Webseite/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Athenathon_Webseite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Athenathon_Webseite.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<User> Users { get; set; }
+    }
+}
