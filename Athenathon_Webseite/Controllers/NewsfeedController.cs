d5a5a7ee1d9acda51ca9e72403a9ed1544a26f50
Anne Schmallenbach <anne_everdeen@gmx.de>
2021-07-21 23:52:50 +0200

    minor corrections + Dashboard hardcoded Version 1

--- Athenathon_Webseite/Controllers/NewsfeedController.cs
+++ Athenathon_Webseite/Controllers/NewsfeedController.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-//in Arbeit!
+
 namespace Athenathon_Webseite.Controllers
 {
     public class NewsfeedController : Controller
