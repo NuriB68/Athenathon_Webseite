6e19cfbae042426b94de0cfa3ecd75bb1c158c48
Anne Schmallenbach <anne_everdeen@gmx.de>
2021-07-22 18:58:54 +0200

    Dashboard MockUp hardgecoded mit Message für Dozent

--- Athenathon_Webseite/Controllers/DashboardController.cs
+++ Athenathon_Webseite/Controllers/DashboardController.cs
@@ -9,6 +9,7 @@ using Athenathon_Webseite.Models;
 using Microsoft.AspNetCore.Authorization;
 
 
+
 namespace Athenathon_Webseite.Controllers
 {
     public class DashboardController : Controller
@@ -18,6 +19,22 @@ namespace Athenathon_Webseite.Controllers
         {
             _db = db;
         }
+        
+        /* Nach 5 Tagen ausgiebigem Probieren (10h je) habe ich die Datenbankanbindung für das Dashboard nicht auf die 
+         * Reihe gekriegt. Theoretisch sollte es so funktionieren:
+         * 1. WebMethod erstellen (um Google Chart mit Werten aus der Datenbank zu füllen.
+         * Query zieht Daten, Ergebnisse werden in Array überführt, und dann als Liste von Objekten weitergegeben
+         * 2. Im Index View des Dashboards zuerst die Google Chart API integrieren, dann drawChart Methode nutzen um mittels
+         * jQuery AJAX die WebMethod zu initialisieren. Chart einem Div ELement zuweisen.
+         * 
+         * Leider war ich durch andere universitäre Verpflichtungen daran gebunden, meine Teilaufgaben erst im Juli
+         * erledigen zu können. Mit mehr Zeit bin ich zuversichtlich, das ich die Datenbankanbindung noch hinbekommen hätte.
+         * So habe ich mich für das Minimum Viable Product eines MockUp Dashboards entschieden, in dem die Daten hart gecoded sind
+         * aber zumindest das Design des Dashboards fertig und sichtbar ist.
+         * Ich möchte sie bitten meine mangelnde Zeitplanung nicht meinen Kommilitonen zu Last zu legen.
+         * Mit freundlichen Grüßen
+         * Anne Schmallenbach
+         */
         
         public IActionResult Index()
         {
